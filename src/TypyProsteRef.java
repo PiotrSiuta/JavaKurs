0bd022638b0f95a737d3612b8d0f7c06915a495f
Piotrek Tester Automatyczny <p.siuta91@gmail.com>
2022-04-15 15:35:34 +0200

    sample commit message

---
+++ src/TypyProsteRef.java
@@ -0,0 +1,7 @@
+public class TypyProsteRef {
+    public static void main(String[] args) {
+        String hello = "cześć Piotrek";
+
+        System.out.println(hello.charAt(1));
+    }
+}
---
+++ src/Zmienne.java
@@ -0,0 +1,17 @@
+//zarezerwowane miejsce w pamieci z okreslonym typem danych
+
+public class Zmienne {
+    public static void main(String[] args) {
+        int firstNumber, secondNumber, result;
+
+        String name = "Piotrek";
+        firstNumber = 1;
+        secondNumber = 2;
+        result = firstNumber + secondNumber;
+
+        name = "Piotrek2";
+        System.out.println(name);
+        System.out.println(result);
+
+    }
+}
